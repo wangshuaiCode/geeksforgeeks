b14bffe959994c8a95fe94413f25b99c38428fa4
wangshuaiCode <wangshuaimail001@gmail.com>
2014-10-09 23:42:14 +0800

    14/10/09 second solution

--- bintree/inorderSuccessor.cpp
+++ bintree/inorderSuccessor.cpp
@@ -29,3 +29,33 @@ struct node *minvalue(struct node *root)
 	
 	return current;
 }
+
+/*the second solution
+ * do not need a parent pointer
+ */
+struct node {
+	int data;
+	struct node *left;
+	struct node *right;
+};
+struct node *inorderSuccessor(struct node *root, struct node *n)
+{
+	if(n -> right != NULL)
+	    return minvalue(n -> right);
+	struct node *succ = NULL;
+	while(root != NULL)
+	{
+		if(n -> data < root -> data) {
+			succ = root;
+			root = root -> left;
+		} else if(n -> data > root -> data) {
+			succ = root;
+			root = root -> right;
+		}
+
+		else
+			break;
+	}
+	return succ;
+
+}
